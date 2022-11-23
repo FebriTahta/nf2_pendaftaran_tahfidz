ff43406594c1376d63153919ee1f1aa38d2c9f87
FebriTahta <febrirtah@gmail.com>
2022-11-23 15:15:18 +0700

    rev brosur 1

--- resources/views/fe_page/formpendaftaran.blade.php
+++ resources/views/fe_page/formpendaftaran.blade.php
@@ -180,7 +180,7 @@
                                     for="property-title"> Alamat Asal Sekolah</label> --}}
                                 <input id="property-title" name="santri_asalsekolah"
                                     class="font-light w-full leading-[1.75] placeholder:opacity-100 placeholder:text-body border border-primary border-opacity-60 rounded-[8px] p-[15px] focus:border-[#FD6400] focus:border-opacity-60 focus:outline-none focus:drop-shadow-[0px_6px_15px_rgba(0,0,0,0.1)] h-[60px] "
-                                    type="text" placeholder="Asal Sekolah ...">
+                                    type="text" placeholder="Alamat Asal Sekolah ...">
                             </div>
                         </div>
 
